11683c69366787fd91508fac21d48897e7c22b38
Guillaume Roussin <guillaume.roussin44@gmail.com>
2022-04-01 19:45:49 +0200

    Added desktop warning banner

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <div
+    class="m-3 mb-5 hidden items-center gap-3 rounded-md bg-red-500 p-3 font-bold text-white lg:flex"
+  >
+    <VueFeather type="alert-triangle" />
+    <h1>
+      <b>SuperEDT</b> n'est pas actuellement prévu pour ordinateur. Favorisez la
+      vue mobile
+    </h1>
+  </div>
+
   <router-view
     id="router"
     class="relative overflow-y-scroll px-5 pb-0 pt-5"
@@ -34,5 +44,11 @@ body {
 #app {
   display: grid;
   grid-template-rows: 1fr auto;
+}
+
+@media (min-width: 1024px) {
+  #app {
+    grid-template-rows: auto 1fr auto;
+  }
 }
 </style>
