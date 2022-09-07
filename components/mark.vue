f0b655bfbaaa76be76446c1e9dbcd276ef02e804
Julian TRANI <73946832+JulianT31@users.noreply.github.com>
2022-09-07 22:08:48 +0200

    add mark component

---
+++ components/mark.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card" @click="hide">
+    Anglais - Présentation
+    <div class="mark">
+      <div class="your_mark">15</div>
+      /20
+    </div>
+    {{ isHidden }}
+    <div v-if="isHidden"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// reactive state
+var isHidden = ref(false);
+
+// functions that mutate state and trigger updates
+function hide() {
+  isHidden.value = !isHidden.value;
+  console.log("yo");
+}
+</script>
+
+<style scoped>
+.card {
+  justify-content: space-between;
+}
+
+.mark {
+  display: flex;
+  justify-content: space-between;
+}
+
+.your_mark {
+  color: var(--primary);
+}
+</style>
--- pages/marks.vue
+++ pages/marks.vue
@@ -4,9 +4,16 @@
       <vue-feather type="feather"></vue-feather>
       <h1 class="text-base font-bold text-primary">SuperEDT</h1>
     </div>
+
+    <div class="text-base text-primary">Dernière notes</div>
+    <Mark />
+    <div class="flex w-full rounded border border-primary"></div>
+    <Mark v-for="i in new Array(5)" />
   </nuxt-layout>
 </template>
 
 <script setup lang="ts">
 import VueFeather from "vue-feather";
 </script>
+
+<style></style>
